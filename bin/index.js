aed8d361ff3a56e3b0445aee52fa443ccd40249e
Sander <sander@bonge.rs>
2021-09-06 14:03:33 +0200

    Renamed Goodsreads and Bol.com instances variables

--- bin/index.js
+++ bin/index.js
@@ -4,8 +4,8 @@ const { program } = require('commander')
 const Goodreads = require('../lib/Goodreads')
 const Bolcom = require('../lib/Bol')
 
-const goodreads = new Goodreads()
-const bol = new Bolcom()
+const goodreadsInstance = new Goodreads()
+const bolInstance = new Bolcom()
 
 program
   .option('-s, --sort <property> <order>', 'The property and order to sort by. E.g. --sort price asc', 'rating desc')
@@ -16,9 +16,9 @@ const sortProperty = sortOption[0]
 const orderValue1 = sortOption[1] === 'desc' ? -1 : 1
 const orderValue2 = sortOption[1] === 'desc' ? 1 : -1
 
-goodreads.getBooks().then(() => {
-  bol.books = goodreads.books
-  bol.searchBooks().then(() => {
-    console.log(bol.books.sort((a, b) => a[sortProperty] > b[sortProperty] ? orderValue1 : orderValue2))
+goodreadsInstance.getBooks().then(() => {
+  bolInstance.books = goodreadsInstance.books
+  bolInstance.searchBooks().then(() => {
+    console.log(bolInstance.books.sort((a, b) => a[sortProperty] > b[sortProperty] ? orderValue1 : orderValue2))
   })
 })
